50f27c3624d75818b1e9205d7d6ff74d2012d893
ftilde <ftilde@protonmail.com>
2017-07-11 14:46:31 +0200

    Add dummy implementations for ansi Handler to TerminalWindow

--- unsegen/terminal/src/terminalwindow.rs
+++ unsegen/terminal/src/terminalwindow.rs
@@ -19,6 +19,7 @@ use unsegen::input::{
     OperationResult,
 };
 use ansi::{
+    CursorStyle,
     Handler,
     TermInfo,
 };
@@ -189,12 +190,96 @@ impl Widget for TerminalWindow {
 
 impl Handler for TerminalWindow {
 
+    /// OSC to set window title
+    fn set_title(&mut self, _: &str) {
+        //TODO: (Although this might not make sense to implement. Do we want to display a title?)
+    }
+
+    /// Set the cursor style
+    fn set_cursor_style(&mut self, _: CursorStyle) {
+        //TODO
+    }
+
     /// A character to be displayed
     fn input(&mut self, c: char) {
         self.with_cursor(|cursor| {
             write!(cursor, "{}", c).unwrap();
         });
     }
+
+    /*
+    /// Set cursor to position
+    fn goto(&mut self, Line, Column) {
+        //TODO
+    }
+
+    /// Set cursor to specific row
+    fn goto_line(&mut self, Line) {
+        //TODO
+    }
+
+    /// Set cursor to specific column
+    fn goto_col(&mut self, Column) {
+        //TODO
+    }
+
+    /// Insert blank characters in current line starting from cursor
+    fn insert_blank(&mut self, Column) {
+        //TODO
+    }
+
+    /// Move cursor up `rows`
+    fn move_up(&mut self, Line) {
+        //TODO
+    }
+
+    /// Move cursor down `rows`
+    fn move_down(&mut self, Line) {
+        //TODO
+    }
+
+    /// Identify the terminal (should write back to the pty stream)
+    ///
+    /// TODO this should probably return an io::Result
+    fn identify_terminal<W: io::Write>(&mut self, &mut W) {
+        //TODO
+    }
+
+    /// Report device status
+    fn device_status<W: io::Write>(&mut self, &mut W, usize) {
+        //TODO
+    }
+
+    /// Move cursor forward `cols`
+    fn move_forward(&mut self, Column) {
+        //TODO
+    }
+
+    /// Move cursor backward `cols`
+    fn move_backward(&mut self, Column) {
+        //TODO
+    }
+
+    /// Move cursor down `rows` and set to column 1
+    fn move_down_and_cr(&mut self, Line) {
+        //TODO
+    }
+
+    /// Move cursor up `rows` and set to column 1
+    fn move_up_and_cr(&mut self, Line) {
+        //TODO
+    }
+
+    /// Put `count` tabs
+    fn put_tab(&mut self, _count: i64) {
+        //TODO
+    }
+
+    /// Backspace `count` characters
+    fn backspace(&mut self) {
+        //TODO
+    }
+    */
 
     /// Carriage return
     fn carriage_return(&mut self) {
@@ -209,6 +294,169 @@ impl Handler for TerminalWindow {
             cursor.wrap_line()
         });
     }
+
+    /// Ring the bell
+    fn bell(&mut self) {
+        //omitted
+    }
+
+    /*
+    /// Substitute char under cursor
+    fn substitute(&mut self) {
+        //TODO
+    }
+
+    /// Newline
+    fn newline(&mut self) {
+        //TODO
+    }
+
+    /// Set current position as a tabstop
+    fn set_horizontal_tabstop(&mut self) {
+        //TODO
+    }
+
+    /// Scroll up `rows` rows
+    fn scroll_up(&mut self, Line) {
+        //TODO
+    }
+
+    /// Scroll down `rows` rows
+    fn scroll_down(&mut self, Line) {
+        //TODO
+    }
+
+    /// Insert `count` blank lines
+    fn insert_blank_lines(&mut self, Line) {
+        //TODO
+    }
+
+    /// Delete `count` lines
+    fn delete_lines(&mut self, Line) {
+        //TODO
+    }
+
+    /// Erase `count` chars in current line following cursor
+    ///
+    /// Erase means resetting to the default state (default colors, no content,
+    /// no mode flags)
+    fn erase_chars(&mut self, Column) {
+        //TODO
+    }
+
+    /// Delete `count` chars
+    ///
+    /// Deleting a character is like the delete key on the keyboard - everything
+    /// to the right of the deleted things is shifted left.
+    fn delete_chars(&mut self, Column) {
+        //TODO
+    }
+
+    /// Move backward `count` tabs
+    fn move_backward_tabs(&mut self, _count: i64) {
+        //TODO
+    }
+
+    /// Move forward `count` tabs
+    fn move_forward_tabs(&mut self, _count: i64) {
+        //TODO
+    }
+
+    /// Save current cursor position
+    fn save_cursor_position(&mut self) {
+        //TODO
+    }
+
+    /// Restore cursor position
+    fn restore_cursor_position(&mut self) {
+        //TODO
+    }
+
+    /// Clear current line
+    fn clear_line(&mut self, _mode: LineClearMode) {
+        //TODO
+    }
+
+    /// Clear screen
+    fn clear_screen(&mut self, _mode: ClearMode) {
+        //TODO
+    }
+
+    /// Clear tab stops
+    fn clear_tabs(&mut self, _mode: TabulationClearMode) {
+        //TODO
+    }
+
+    /// Reset terminal state
+    fn reset_state(&mut self) {
+        //TODO
+    }
+
+    /// Reverse Index
+    ///
+    /// Move the active position to the same horizontal position on the
+    /// preceding line. If the active position is at the top margin, a scroll
+    /// down is performed
+    fn reverse_index(&mut self) {
+        //TODO
+    }
+
+    /// set a terminal attribute
+    fn terminal_attribute(&mut self, _attr: Attr) {
+        //TODO
+    }
+
+    /// Set mode
+    fn set_mode(&mut self, _mode: Mode) {
+        //TODO
+    }
+
+    /// Unset mode
+    fn unset_mode(&mut self, Mode) {
+        //TODO
+    }
+
+    /// DECSTBM - Set the terminal scrolling region
+    fn set_scrolling_region(&mut self, Range<Line>) {
+        //TODO
+    }
+
+    /// DECKPAM - Set keypad to applications mode (ESCape instead of digits)
+    fn set_keypad_application_mode(&mut self) {
+        //TODO
+    }
+
+    /// DECKPNM - Set keypad to numeric mode (digits intead of ESCape seq)
+    fn unset_keypad_application_mode(&mut self) {
+        //TODO
+    }
+
+    /// Set one of the graphic character sets, G0 to G3, as the active charset.
+    ///
+    /// 'Invoke' one of G0 to G3 in the GL area. Also refered to as shift in,
+    /// shift out and locking shift depending on the set being activated
+    fn set_active_charset(&mut self, CharsetIndex) {
+        //TODO
+    }
+
+    /// Assign a graphic character set to G0, G1, G2 or G3
+    ///
+    /// 'Designate' a graphic character set as one of G0 to G3, so that it can
+    /// later be 'invoked' by `set_active_charset`
+    fn configure_charset(&mut self, CharsetIndex, StandardCharset) {
+        //TODO
+    }
+
+    /// Set an indexed color value
+    fn set_color(&mut self, usize, Rgb) {
+        //TODO
+    }
+
+    /// Run the dectest routine
+    fn dectest(&mut self) {
+        //TODO
+    }
+    */
 }
 
 impl TermInfo for TerminalWindow {

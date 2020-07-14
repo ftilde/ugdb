261dbaf98640c2a2ced39324501f8665b7717c2d
ftilde <ftilde@tamepointer.de>
2020-07-14 22:01:24 +0200

    Fix build script when run outside of git repository

--- build.rs
+++ build.rs
@@ -3,21 +3,25 @@ extern crate toml;
 
 use toml::{from_str, Value};
 
+fn get_revision() -> Option<String> {
+    let output = std::process::Command::new("git")
+        .args(&["rev-parse", "HEAD"])
+        .output()
+        .ok()?;
+    if !output.status.success() {
+        return None;
+    }
+    let prefix = &output.stdout[..8];
+    Some(String::from_utf8(prefix.to_vec()).unwrap())
+}
+
 fn main() {
     // Preprocess lalrpop grammar files
     lalrpop::process_root().unwrap();
     println!("cargo:rerun-if-changed=src/gdb_expression_parsing/parser.lalrpop");
 
     // Find git revision of current version, if possible
-    let revision_str = if let Ok(output) = std::process::Command::new("git")
-        .args(&["rev-parse", "HEAD"])
-        .output()
-    {
-        let prefix = &output.stdout[..8];
-        String::from_utf8(prefix.to_vec()).unwrap()
-    } else {
-        " release".to_owned()
-    };
+    let revision_str = get_revision().unwrap_or_else(|| " release".to_owned());
     println!("cargo:rustc-env=REVISION={}", revision_str);
     // The .git directory mtime should change if something is commited, so we rerun the build
     // script in that case to update the revision.
